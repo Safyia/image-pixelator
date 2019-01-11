056d34df303bd37f96e38e6d7bd06e2d494d92aa
john p <john.pendexter@gmail.com>
2019-01-10 22:43:58 -0500

    Switched out readLines for lineIterator so the program can handle larger input files than what is provided here... also nudged the threshold on memory down just a tad.

--- src/main/java/com/github/pendext/image/output/ImagePixelator.java
+++ src/main/java/com/github/pendext/image/output/ImagePixelator.java
@@ -1,7 +1,7 @@
 package com.github.pendext.image.output;
 
 import com.github.pendext.image.queue.*;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.*;
 
 import java.io.*;
 import java.net.*;
@@ -10,22 +10,22 @@ import java.util.concurrent.*;
 
 public class ImagePixelator {
 
-    private static final int FILE_BATCH_SIZE = 8000000;
+    private static final int FILE_BATCH_SIZE = 8040000;
 
     public static void main(String[] args) throws IOException {
         System.out.println("Beginning process to determine image pixels");
         URL input = ImagePixelator.class.getResource("/input.txt");
         File file = new File(input.getFile());
 
-        List<String> urls = FileUtils.readLines(file, "UTF-8");
-
+        LineIterator iterator = FileUtils.lineIterator(file, "UTF-8");
         BlockingQueue<List<URL>> queue = new LinkedBlockingQueue<>();
-
         int fileSizes = 0;
         List<URL> urlsToProcess = new ArrayList<>();
-        for (String url : urls) {
-            try {
-                URL imageUrl = new URL(url);
+
+        try {
+            while (iterator.hasNext()) {
+                String currentUrl = iterator.next();
+                URL imageUrl = new URL(currentUrl);
                 urlsToProcess.add(imageUrl);
 
                 URLConnection urlConnection = imageUrl.openConnection();
@@ -39,9 +39,9 @@ public class ImagePixelator {
                     urlsToProcess.clear();
                     fileSizes = 0;
                 }
-            } catch (Exception e) {
-                throw new RuntimeException("Something went wrong pixeling images", e);
             }
+        } catch (Exception e) {
+            throw new RuntimeException("Something went wrong pixeling images", e);
         }
 
         System.out.println("Completed process to determine image pixels");
